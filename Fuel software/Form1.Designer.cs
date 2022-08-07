21b3d2158e0a56e48a6c97c3455e9200d4039b26
Meshydra <71543277+Meshydra@users.noreply.github.com>
2022-08-07 15:38:58 +0530

    Add files via upload

---
+++ Fuel software/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Fuel_software
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnCalender = new System.Windows.Forms.Button();
+            this.btnAboutUs = new System.Windows.Forms.Button();
+            this.lnkLogin = new System.Windows.Forms.LinkLabel();
+            this.bxLogo = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnKandy = new System.Windows.Forms.Button();
+            this.btnColombo = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.gbxMainMenu = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.gbxMainMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnHome
+            // 
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHome.Location = new System.Drawing.Point(26, 62);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(141, 41);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnList
+            // 
+            this.btnList.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnList.Location = new System.Drawing.Point(26, 130);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(141, 41);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "Price List";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // btnApply
+            // 
+            this.btnApply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnApply.Location = new System.Drawing.Point(26, 198);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(141, 41);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Apply for Fuel";
+            this.btnApply.UseVisualStyleBackColor = true;
+            // 
+            // btnCalender
+            // 
+            this.btnCalender.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalender.Location = new System.Drawing.Point(26, 262);
+            this.btnCalender.Name = "btnCalender";
+            this.btnCalender.Size = new System.Drawing.Size(141, 41);
+            this.btnCalender.TabIndex = 3;
+            this.btnCalender.Text = "Calender";
+            this.btnCalender.UseVisualStyleBackColor = true;
+            // 
+            // btnAboutUs
+            // 
+            this.btnAboutUs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAboutUs.Location = new System.Drawing.Point(26, 332);
+            this.btnAboutUs.Name = "btnAboutUs";
+            this.btnAboutUs.Size = new System.Drawing.Size(141, 41);
+            this.btnAboutUs.TabIndex = 4;
+            this.btnAboutUs.Text = "About Us";
+            this.btnAboutUs.UseVisualStyleBackColor = true;
+            // 
+            // lnkLogin
+            // 
+            this.lnkLogin.AutoSize = true;
+            this.lnkLogin.Location = new System.Drawing.Point(26, 404);
+            this.lnkLogin.Name = "lnkLogin";
+            this.lnkLogin.Size = new System.Drawing.Size(40, 15);
+            this.lnkLogin.TabIndex = 5;
+            this.lnkLogin.TabStop = true;
+            this.lnkLogin.Text = "Log in";
+            // 
+            // bxLogo
+            // 
+            this.bxLogo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bxLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bxLogo.BackgroundImage")));
+            this.bxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bxLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bxLogo.Location = new System.Drawing.Point(75, 8);
+            this.bxLogo.Name = "bxLogo";
+            this.bxLogo.Size = new System.Drawing.Size(121, 110);
+            this.bxLogo.TabIndex = 6;
+            this.bxLogo.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(202, 18);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(356, 86);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Fuel Lanka";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 8);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 108);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnKandy
+            // 
+            this.btnKandy.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnKandy.Location = new System.Drawing.Point(4, 122);
+            this.btnKandy.Name = "btnKandy";
+            this.btnKandy.Size = new System.Drawing.Size(141, 41);
+            this.btnKandy.TabIndex = 9;
+            this.btnKandy.Text = "Kandy";
+            this.btnKandy.UseVisualStyleBackColor = true;
+            this.btnKandy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnColombo
+            // 
+            this.btnColombo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnColombo.Location = new System.Drawing.Point(4, 190);
+            this.btnColombo.Name = "btnColombo";
+            this.btnColombo.Size = new System.Drawing.Size(141, 41);
+            this.btnColombo.TabIndex = 10;
+            this.btnColombo.Text = "Colombo";
+            this.btnColombo.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(4, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 41);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Galle";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(4, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 41);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Jaffna";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 404);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 97);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(6, 124);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(667, 231);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // gbxMainMenu
+            // 
+            this.gbxMainMenu.Controls.Add(this.lnkLogin);
+            this.gbxMainMenu.Controls.Add(this.btnAboutUs);
+            this.gbxMainMenu.Controls.Add(this.btnCalender);
+            this.gbxMainMenu.Controls.Add(this.btnApply);
+            this.gbxMainMenu.Controls.Add(this.btnList);
+            this.gbxMainMenu.Controls.Add(this.btnHome);
+            this.gbxMainMenu.Location = new System.Drawing.Point(12, 12);
+            this.gbxMainMenu.Name = "gbxMainMenu";
+            this.gbxMainMenu.Size = new System.Drawing.Size(207, 541);
+            this.gbxMainMenu.TabIndex = 15;
+            this.gbxMainMenu.TabStop = false;
+            this.gbxMainMenu.Text = "Main Menu";
+            this.gbxMainMenu.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.lblTitle);
+            this.groupBox1.Controls.Add(this.bxLogo);
+            this.groupBox1.Location = new System.Drawing.Point(229, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(689, 541);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.btnColombo);
+            this.groupBox2.Controls.Add(this.btnKandy);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(929, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(157, 541);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1101, 565);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxMainMenu);
+            this.Name = "Form1";
+            this.Text = "Fuel Software";
+            ((System.ComponentModel.ISupportInitialize)(this.bxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.gbxMainMenu.ResumeLayout(false);
+            this.gbxMainMenu.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnHome;
+        private Button btnList;
+        private Button btnApply;
+        private Button btnCalender;
+        private Button btnAboutUs;
+        private LinkLabel lnkLogin;
+        private PictureBox bxLogo;
+        private Label lblTitle;
+        private PictureBox pictureBox2;
+        private Button btnKandy;
+        private Button btnColombo;
+        private Button button3;
+        private Button button4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private GroupBox gbxMainMenu;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+    }
+}
---
+++ Fuel software/Form1.cs
@@ -0,0 +1,20 @@
+namespace Fuel_software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
